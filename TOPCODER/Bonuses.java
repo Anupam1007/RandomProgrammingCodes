b593bb5e968689f852cd5a0dfa2f3ae7b235f229
Anupam1007 <anupamt62@gmail.com>
2017-08-27 12:35:59 +0530

    wrong answer on topcoder, mine is correct

--- TOPCODER/Bonuses.java
+++ TOPCODER/Bonuses.java
@@ -8,7 +8,8 @@ class Bonuses
 		int size=input.length;
 		int sum=0;
 		boolean isPointSame=true;
-		double sumOfDec=0.0d;
+		int sumOfDec=0;
+		//int truncateGain=0;
 		int ret[]=new int[size];
 		for(int i=0;i<size;i++)
 		{
@@ -16,50 +17,30 @@ class Bonuses
 		}
 		for(int i=0;i<size;i++)
 		{
-			double per=(double)((double)input[i]/(double)sum)*100d;
-			//ret[i]=(int)per;
-			sumOfDec=sumOfDec+(per % 1);
-			ret[i]=(int)(per - per%1);
-			System.out.println(per+"\t\t"+(per % 1)+"\n\n");
+			double per=((double)input[i]/sum)*100d;
+			ret[i]=(int)per;
+			sumOfDec=sumOfDec+ret[i];
 		}
-		// System.out.println("\n\n");
-		// System.out.println(Arrays.toString(ret));
-		// System.out.println("\n\n");
-		//System.out.println(Math.ceil(sumOfDec)+"\n\n");
-		sumOfDec=Math.ceil(sumOfDec);
-		
-		//if(isPointSame)
-		//{
-			// int i=0;
-			// while(sumOfDec>0)
-			// {
-				// ret[i]=ret[i] + 1;
-				// i++;
-				// sumOfDec--;
-			// }
-		// }
-		//else
-		//{
-			int[] track=new int[size];
-			int max=0,index=-1;
-			while(sumOfDec>0)
+		sumOfDec=100-sumOfDec;
+		System.out.println(sumOfDec);
+		System.out.println(Arrays.toString(ret));
+		int[] track=new int[size];
+		int max=0,index=-1;
+		while(sumOfDec>0)
+		{
+			for(int i=0;i<size;i++)
 			{
-				for(int i=0;i<size;i++)
+				if(ret[i]>max && track[i]!=-1)
 				{
-					if(ret[i]>max && track[i]!=-1)
-					{
-						max=ret[i];
-						index=i;
-					}
+					max=ret[i];
+					index=i;
 				}
-				track[index]=-1;
-				ret[index]++;
-				sumOfDec--;
-				max=0;
 			}
-		//}
-		//System.out.println(Arrays.toString(ret));
-		
+			track[index]=-1;
+			ret[index]++;
+			sumOfDec--;
+			max=0;
+		}
 		return ret;
 	}
 	
@@ -69,3 +50,60 @@ class Bonuses
 		System.out.print(Arrays.toString(Bonuses.getDivision(arr)));
 	}
 }
+
+//[8, 6, 5, 2, 8, 5, 5, 3, 1, 5, 4, 4, 6, 3, 4, 4, 1, 8, 1, 6, 3, 8]
+//[8, 6, 5, 2, 8, 5, 5, 3, 1, 5, 4, 4, 6, 3, 4, 4, 1, 8, 1, 6, 3, 8]
+
+// import java.util.Comparator;
+// import java.util.*;
+
+// public class Bonuses{
+  // public  int[] getDivision(int[] points){
+		// int totalPoints = 0;    
+		// for(int num: points)
+	    	// totalPoints += num;
+    
+		// int[] bonuses = new int[points.length];
+		// PriorityQueue<Tuple> maxHeap = new PriorityQueue<Tuple>(points.length, new MyComparator());
+
+   		// int pLeft = 100;
+   		// for(int i=0; i < points.length; i++){
+     		// bonuses[i] = (points[i] * 100) / totalPoints;
+     		// maxHeap.add(new Tuple(bonuses[i],i));
+     		// pLeft -= bonuses[i];    		
+     	// }   
+
+  		 // /*Start of adding the extra bit of % to the top employees */
+   		// while(pLeft > 0){
+     		// ++bonuses[maxHeap.poll().val2];
+	       	// --pLeft;
+       	// }  	
+   		// return bonuses;
+ 	// }
+	
+	// public class Tuple{
+		// public int val1;
+		// public int val2;
+		// public Tuple(int a, int b){
+			// this.val1 = a;
+			// this.val2 = b;
+		// }
+	// }
+	
+	// public class MyComparator implements Comparator<Tuple>
+	// {
+    	// public int compare( Tuple x, Tuple y )
+	    // {
+    	    // if(x.val1 != y.val1)
+	    	    // return y.val1 - x.val1;
+	    	// return  x.val2 - y.val2;
+	    // }
+	// }
+	
+	// public static void main(String args[])
+	// {
+		// int[] arr={485, 324, 263, 143, 470, 292, 304, 188, 100, 254, 296,255, 360, 231, 311, 275,  93, 463, 115, 366, 197, 470};
+		// Bonuses b=new Bonuses();
+		// System.out.print(Arrays.toString(b.getDivision(arr)));
+	// }
+// }
